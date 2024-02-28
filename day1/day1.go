d15c686198e5ab2e08970edf50c865843523078b
Rodrigo Couto <r@rodg.co>
2024-02-28 09:04:26 -0300

    Refactor day1

--- day1/day1.go
+++ day1/day1.go
@@ -1,88 +1,86 @@
 package main
 
 import (
-  "fmt"
-  "bufio"
-  "log"
-  "os"
-  "regexp"
-  "strconv"
+	"fmt"
+	"bufio"
+	"log"
+	"os"
+	"strings"
+	"strconv"
+	"regexp"
 )
 
+func calibrate(str string) (calibration int) {
+	firstIndex := strings.IndexAny(str, "123456789")
+	firstDigit, _ := strconv.Atoi(string(str[firstIndex]))
+
+	lastIndex := strings.LastIndexAny(str, "123456789")
+	lastDigit, _ := strconv.Atoi(string(str[lastIndex]))
+
+	calibration = firstDigit*10 + lastDigit
+	return
+}
+
 func part1(s *bufio.Scanner) (total int) {
-  var firstDigit, lastDigit int
 	for s.Scan() {
 		str := s.Text()
-
-    reg := regexp.MustCompile(`^.*?(\d{1}).*(\d{1}).*$|^.*(\d{1}).*$`)
-    matches := reg.FindStringSubmatch(str)
-
-    if matches[1] == "" {
-      firstDigit, _ = strconv.Atoi(matches[3])
-      lastDigit, _ = strconv.Atoi(matches[3])
-    } else {
-      firstDigit, _ = strconv.Atoi(matches[1])
-      lastDigit, _ = strconv.Atoi(matches[2])
-    }
-
-    calibration := firstDigit*10 + lastDigit
-
-    total += calibration
-  }
-  return total
+		calibration := calibrate(str)
+		total += calibration
+	}
+	return
 }
 
 func stringToDigit(str string) (digit int) {
-  switch str {
-  case "one", "1":
-    digit = 1
-  case "two", "2":
-    digit = 2
-  case "three", "3":
-    digit = 3
-  case "four", "4":
-    digit = 4
-  case "five", "5":
-    digit = 5
-  case "six", "6":
-    digit = 6
-  case "seven", "7":
-    digit = 7
-  case "eight", "8":
-    digit = 8
-  case "nine", "9":
-    digit = 9
-  }
-  return digit
+	switch str {
+	case "one", "1":
+		digit = 1
+	case "two", "2":
+		digit = 2
+	case "three", "3":
+		digit = 3
+	case "four", "4":
+		digit = 4
+	case "five", "5":
+		digit = 5
+	case "six", "6":
+		digit = 6
+	case "seven", "7":
+		digit = 7
+	case "eight", "8":
+		digit = 8
+	case "nine", "9":
+		digit = 9
+	}
+	return
 }
-// [0-9]|one|two|three|four|five|six|seven|eight|nine
+
 func part2(s *bufio.Scanner) (total int) {
-  var firstDigit, lastDigit int
+	var reg *regexp.Regexp
+	var matches []string
+	var firstDigit, lastDigit int
+
 	for s.Scan() {
 		str := s.Text()
 
-    reg := regexp.MustCompile(`^.*?([0-9]|one|two|three|four|five|six|seven|eight|nine).*([0-9]|one|two|three|four|five|six|seven|eight|nine).*$|^.*([0-9]|one|two|three|four|five|six|seven|eight|nine).*$`)
-    matches := reg.FindStringSubmatch(str)
+		reg = regexp.MustCompile(`^.*?([0-9]|one|two|three|four|five|six|seven|eight|nine)`)
+		matches = reg.FindStringSubmatch(str)
+		firstDigit = stringToDigit(matches[1])
 
-    if matches[1] == "" {
-      firstDigit = stringToDigit(matches[3])
-      lastDigit = stringToDigit(matches[3])
-    } else {
-      firstDigit = stringToDigit(matches[1])
-      lastDigit = stringToDigit(matches[2])
-    }
+		reg = regexp.MustCompile(`^.*([0-9]|one|two|three|four|five|six|seven|eight|nine).*?$`)
+		matches = reg.FindStringSubmatch(str)
+		lastDigit = stringToDigit(matches[1])
 
-    calibration := firstDigit*10 + lastDigit
+		calibration := firstDigit*10 + lastDigit
 
-    total += calibration
-  }
-  return total
+		total += calibration
+	}
+	return
 }
 
 func main() {
-  f, err := os.Open("day1/input.txt")
+	f, err := os.Open("day1/input.txt")
 
-  if err != nil {
+	if err != nil {
 		log.Fatal(err)
 	}
 
